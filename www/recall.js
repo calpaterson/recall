911fcd0aa01a72f7b412d677122c5acb44d2de55
Cal Paterson <cal@calpaterson.com>
2012-03-03 17:11:25 +0530

    Added AGPLv3 boilerplate to all files.  Also added configuration for www

--- www/recall.js
+++ www/recall.js
@@ -1,4 +1,18 @@
-var BASE_URL = "localhost";
+// Recall is a program for storing bookmarks of different things
+// Copyright (C) 2012  Cal Paterson
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 $(document).ready(
     function() {
@@ -45,7 +59,7 @@ $(document).ready(
 
 	// List of posts
 	// -------------
-	$.getJSON("http://" + BASE_URL + "/mark/cal@calpaterson.com",
+	$.getJSON(recall_config["api-base-url"] + "/mark/cal@calpaterson.com",
 		 function (data){
 		     var convertUnixtimeToDate = function(unixtime){
 			 var now = new Date();
