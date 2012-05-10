77920744424ab35b1673ee550dab9230576accc3
Cal Paterson <cal@calpaterson.com>
2012-05-10 19:30:33 +0100

    Adding tornado to replace flasks' own debugging server

--- server/server.py
+++ server/server.py
@@ -328,4 +328,13 @@ def verify_email(email_key):
 
 if __name__ == "__main__":
     load_settings()
-    app.run(debug=settings["RECALL_DEBUG_MODE"])
+    if settings["RECALL_DEBUG_MODE"]:
+        app.run(debug=True)
+    else:
+        from tornado.wsgi import WSGIContainer
+        from tornado.httpserver import HTTPServer
+        from tornado.ioloop import IOLoop
+
+        http_server = HTTPServer(WSGIContainer(app))
+        http_server.listen(5000)
+        IOLoop.instance().start()
