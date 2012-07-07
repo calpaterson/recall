69a9200d49c67dbad97d481d3496c0fdebe3d963
Cal Paterson <cal@calpaterson.com>
2012-07-07 15:01:08 +0100

    Make invite error less misleading

--- www/recall.js
+++ www/recall.js
@@ -145,7 +145,7 @@ core.add(
 
         var failure = function(){
             var button = sandbox.find("#r-i-submit")[0];
-            button.textContent = "Try Again (after filling out all fields)";
+            button.textContent = "Error (try again?)";
             button.classList.remove("disabled");
         };
 
